e3a8df5b1e65b417416706b1f3af35878a29255b
Mario Fragnito <mariofragnitoph@gmail.com>
2024-09-17 14:56:30 +0200

    Update JWTCheckFilter.java

--- src/main/java/fragnito/U5W3D2/security/JWTCheckFilter.java
+++ src/main/java/fragnito/U5W3D2/security/JWTCheckFilter.java
@@ -1,11 +1,16 @@
 package fragnito.U5W3D2.security;
 
+import fragnito.U5W3D2.entities.Dipendente;
+import fragnito.U5W3D2.services.DipendenteService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.UnavailableException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,12 +22,19 @@ public class JWTCheckFilter extends OncePerRequestFilter {
     @Autowired
     private JWTTools jwtTools;
 
+    @Autowired
+    private DipendenteService dipendenteService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) throw new UnavailableException("Inserisci correttamente il token nell'autorizzasione");
         String accessToken = authHeader.substring(7);
         jwtTools.verifyToken(accessToken);
+        String id = jwtTools.extractIdFromToken(accessToken);
+        Dipendente currentUser = this.dipendenteService.getDipendenteById(Integer.parseInt(id));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(currentUser, null, currentUser.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
 
